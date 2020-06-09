21e364d7a8cc0e03b5fd884d3f050c776b61f797
sivamamidi-REISys <siva.mamidi@reisystems.com>
2020-06-09 06:41:51 -0400

    added merging

--- links.js
+++ links.js
@@ -1,4 +1,9 @@
 exports.configuration = {
-    destinationPath: 'c:/Users/siva.mamidi/Downloads/processed',
-     links:[]
-}
+    destinationPath: 'c:/Users/siva.mamidi/Downloads/challenge-what-am-i',
+     links:
+     
+     [
+        
+        "https://www.superteacherworksheets.com/challenges/challengeA1-flag_WQZRF.pdf?up=1466611200","https://www.superteacherworksheets.com/challenges/challengeA2-popcorn_WQZRQ.pdf?up=1466611200","https://www.superteacherworksheets.com/challenges/challengeA3-vet_WQZRM.pdf?up=1466611200","https://www.superteacherworksheets.com/challenges/challengeA4-raisin_WQZRN.pdf?up=1466611200","https://www.superteacherworksheets.com/challenges/challengeA5-barn_WQZRR.pdf?up=1466611200","https://www.superteacherworksheets.com/challenges/challengeA6-egg_WQZRT.pdf?up=1466611200","https://www.superteacherworksheets.com/challenges/challengeA7-tree_WQZRW.pdf?up=1466611200","https://www.superteacherworksheets.com/challenges/challengeB1-whitehouse_WQZTB.pdf?up=1466611200","https://www.superteacherworksheets.com/challenges/challengeB2-watermelon_WQZTD.pdf?up=1466611200","https://www.superteacherworksheets.com/challenges/challengeB3-sand_WQZTF.pdf?up=1481031484","https://www.superteacherworksheets.com/challenges/challengeB4-home_WQZTQ.pdf?up=1466611200","https://www.superteacherworksheets.com/challenges/challengeB5-marshmallows_WQZTM.pdf?up=1481031422","https://www.superteacherworksheets.com/challenges/challengeB6-ants_WQZTN.pdf?up=1466611200","https://www.superteacherworksheets.com/challenges/challengeB7-skunk_WQZTR.pdf?up=1466611200",
+    ]
+    }    
